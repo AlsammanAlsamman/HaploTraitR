a6174b6d9d39b2675ebf0263fd0d42d87dfd0397
AlsammanAlsamman <smahmoud@ageri.sci.eg>
2024-11-03 05:37:04 +0200

    first commit

---
+++ R/hello.R
@@ -0,0 +1,7 @@
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
