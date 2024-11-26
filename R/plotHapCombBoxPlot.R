e83ea1fdc952a4211a409a9fd18d33b266c50b26
Tamara <tamara.ortizru@gmail.com>
2024-11-26 11:10:22 -0500

    Update plotHapCombBoxPlot.R
    Integrated the for loop from the tutorial (making plots for all SNPs in t_test) into the function. User can now choose to make a plot for a single SNP or all SNPs tested, the file generation is also integrated now.

--- R/plotHapCombBoxPlot.R
+++ R/plotHapCombBoxPlot.R
@@ -1,5 +1,5 @@
 #' Plotting Haplotype Combination Boxplot for specific SNP
-#' @param snp The SNP of interest
+#' @param cls_snp The SNP of interest, if NULL then plots for all SNPs in t_test_snpComp are generated
 #' @param SNPcombTables A list of data frames containing haplotype combinations and phenotype values
 #' @param t_test_snpComp A list of t-test results for haplotype combinations
 #' @importFrom ggplot2 ggplot
@@ -12,27 +12,52 @@
 #' @importFrom ggplot2 geom_point
 #' @import ggpubr
 #' @export
-plotHapCombBoxPlot <- function(cls_snp, SNPcombTables, t_test_snpComp, with_dots=TRUE) {
-  comb_sample <- SNPcombTables[SNPcombTables$SNP == cls_snp, ]
-  comb_sample$comb <- paste0("comb", comb_sample$comb)
-  p <- ggplot(comb_sample, aes(x = comb, y = Pheno)) +
-    geom_violin(aes(fill = comb), alpha = 0.5) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    labs(title = paste("Phenotype Distribution by Haplotype Combination for SNP", cls_snp),
-         x = "Haplotype Combination",
-         y = "Phenotype Value") +
-    theme(legend.position = "none")
-
-  if (with_dots) {
-    p <- p + geom_point(aes(x = comb, y = Pheno, color=Pheno), position = position_jitter(width = 0.1), alpha = 0.5)
-  }
-
-  if ("p.adj.signif" %in% colnames(t_test_snpComp[[cls_snp]])) {
-    p <- p + stat_pvalue_manual(t_test_snpComp[[cls_snp]], label = "p.adj.signif", tip.length = 0.01)
-  }
-
+plotHapCombBoxPlot <- function(cls_snp = NULL, SNPcombTables, t_test_snpComp, with_dots = TRUE, outfolder) {
+  if(is.null(cls_snp)){
+    for (i in names(t_test_snpComp)) {
+      comb_sample <- SNPcombTables[SNPcombTables$SNP == i, ]
+      comb_sample$comb <- paste0("comb", comb_sample$comb)
+      p <- ggplot(comb_sample, aes(x = comb, y = Pheno)) +
+        geom_violin(aes(fill = comb), alpha = 0.5) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+        labs(title = paste("Phenotype Distribution by Haplotype Combination for SNP", i),
+             x = "Haplotype Combination",
+             y = "Phenotype Value") +
+        theme(legend.position = "none")
+    
+      if (with_dots) {
+        p <- p + geom_point(aes(x = comb, y = Pheno, color=Pheno), position = position_jitter(width = 0.1), alpha = 0.5)
+      }
+    
+      if ("p.adj.signif" %in% colnames(t_test_snpComp[[i]])) {
+        p <- p + stat_pvalue_manual(t_test_snpComp[[i]], label = "p.adj.signif", tip.length = 0.01)
+      }
+    
+      p
+      ggplot2::ggsave(file.path(outfolder, paste0(i, "_boxplot.png")), width = 8, height = 6)
+    }
+  }else{
+    comb_sample <- SNPcombTables[SNPcombTables$SNP == cls_snp, ]
+    comb_sample$comb <- paste0("comb", comb_sample$comb)
+    p <- ggplot(comb_sample, aes(x = comb, y = Pheno)) +
+      geom_violin(aes(fill = comb), alpha = 0.5) +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      labs(title = paste("Phenotype Distribution by Haplotype Combination for SNP", cls_snp),
+           x = "Haplotype Combination",
+           y = "Phenotype Value") +
+      theme(legend.position = "none")
+  
+    if (with_dots) {
+      p <- p + geom_point(aes(x = comb, y = Pheno, color=Pheno), position = position_jitter(width = 0.1), alpha = 0.5)
+    }
+  
+    if ("p.adj.signif" %in% colnames(t_test_snpComp[[cls_snp]])) {
+      p <- p + stat_pvalue_manual(t_test_snpComp[[cls_snp]], label = "p.adj.signif", tip.length = 0.01)
+    }
+  
   p
-
+  ggplot2::ggsave(file.path(outfolder, paste0(cls_snp, "_boxplot.png")), width = 8, height = 6)
+  }
 }
 
 
